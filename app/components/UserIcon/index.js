8b5dd1083deb08ab67aacd7e304d168a0bc862f9
kazuumi-hori <kazuumi.hori@spacemarket.co.jp>
2017-05-23 21:17:31 +0900

    猫画像

--- app/components/UserIcon/index.js
+++ app/components/UserIcon/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  Platform,
+} from 'react-native';
 
 import styles from './style';
 
@@ -12,7 +15,7 @@ export default class UserIcon extends Component {
     return (
       <Image
         defaultSource={require('../../resources/images/default_user.png')}
-        source={{ uri: this.props.imageUrl }}
+        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/2016-06-12_Orange_and_white_tabby_cat_born_in_2016_%E8%8C%B6%E3%83%88%E3%83%A9%E7%99%BD%E3%81%AD%E3%81%93%E5%AD%90%E7%8C%AB_2016%E5%B9%B4%E7%94%9F%E3%81%BE%E3%82%8C_DSCF6403%E2%98%86%E5%BD%A1.jpg' }}
         style={styles.image}
       />
     );
