65009035c476b7454d60b8e588425420e6f5d25c
kazuumi-hori <kazuumi.hori@spacemarket.co.jp>
2017-05-24 21:58:31 +0900

    Androidはドロワー化 (ただしオプションはつけてない)

---
+++ app/components/Navigation.android.js
@@ -0,0 +1,12 @@
+import * as Screen from '../config/route';
+import { DrawerNavigator } from 'react-navigation';
+import {
+
+} from 'react-native';
+
+export default DrawerNavigator (
+  Screen.route, {
+    tabBarOptions: {
+      showIcon: true,
+    }
+  });
